6bc20b266dee64dbb16a456e0c170676b1560e4e
Athran Rahman <cengallut@gmail.com>
2024-12-03 15:40:38 +0800

    add header social media links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,42 @@
 "use client";
 
 import styles from "./header.module.css";
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
 
 export function ScratchHeader() {
   return (
     <div className={styles["header"]}>
-      <img src="/icons/roboshop-logo-32.png" alt="" height="32" />
+      <img
+        src={`${import.meta.env.BASE_URL}icons/roboshop-logo-32.png`}
+        alt=""
+        height="32"
+        width="32"
+      />
       <h1 className={styles["header-title"]}>roboshop scratch</h1>
+      <a
+        href="https://www.facebook.com/roboshoptechnologies/"
+        referrerPolicy="no-referrer"
+        target="_blank"
+        title="Roboshop Facebook profile"
+      >
+        <FaFacebook />
+      </a>
+      <a
+        href="https://www.instagram.com/roboshoptechnologies/"
+        referrerPolicy="no-referrer"
+        target="_blank"
+        title="Roboshop Instagram profile"
+      >
+        <FaInstagram />
+      </a>
+      <a
+        href="https://www.tiktok.com/@roboschool"
+        referrerPolicy="no-referrer"
+        target="_blank"
+        title="Roboshop Tiktok profile"
+      >
+        <FaTiktok />
+      </a>
     </div>
   );
 }
